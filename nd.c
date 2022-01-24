60211b280224aa51c81d9f116a5da9373d867397
Pascal Cuoq <pascal-cuoq@users.noreply.github.com>
2022-01-24 09:53:48 +0100

    Update nd.c

--- nd.c
+++ nd.c
@@ -1,21 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int count;
+int count, success;
 
 int main(void) {
   if (malloc(1))
-    printf("allocated 1 at step %d\n", count);
+    success++, printf("allocated 1 at step %d\n", count);
   else
     printf("noallocat 1 at step %d\n", count);
   count++;
   if (malloc(2))
-    printf("allocated 2 at step %d\n", count);
+    success++, printf("allocated 2 at step %d, %d successes\n", count, success);
   else
-    printf("noallocat 2 at step %d\n", count);
+    printf("noallocat 2 at step %d, %d successes\n", count, success);
   count++;
   if (malloc(3))
-    printf("allocated 3 at step %d\n", count);
+    success++, printf("allocated 3 at step %d, %d successes\n", count, success);
   else
-    printf("noallocat 3 at step %d\n", count);
+    printf("noallocat 3 at step %d, %d successes\n", count, success);
 }
